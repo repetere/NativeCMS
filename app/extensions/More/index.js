3636a86846c39b74261e861e705d5b814ffc4016
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-09-20 23:10:24 -0400

    use easy grid

--- app/extensions/More/index.js
+++ app/extensions/More/index.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+//TO DO: https://github.com/clh161/react-native-easy-grid-view
 import React, { Component } from 'react';
 import {
   StyleSheet,
