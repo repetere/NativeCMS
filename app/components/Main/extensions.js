e09baf00bacee5147e996de39180386f42f02d29
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-09-25 12:41:49 -0400

    custom home component

--- app/components/Main/extensions.js
+++ app/components/Main/extensions.js
@@ -5,5 +5,16 @@ import Notifications from '../../extensions/Notifications';
 import Profile from '../../extensions/Profile';
 import Settings from '../../extensions/Settings';
 import Login from '../../extensions/Login';
+import custom_standard_extensions from '../../../content/config/custom_standard_extensions';
 
-export default { Apps,  Home, More, Notifications, Profile, Settings, Login, };
+let Extensions = {
+  Apps: custom_standard_extensions.Apps || Apps,
+  Home: custom_standard_extensions.Home || Home,
+  More: custom_standard_extensions.More || More,
+  Notifications: custom_standard_extensions.Notifications || Notifications,
+  Profile: custom_standard_extensions.Profile || Profile,
+  Settings: custom_standard_extensions.Settings || Settings,
+  Login: custom_standard_extensions.Login || Login,
+};
+
+export default Extensions;
---
+++ content/config/custom_standard_extensions.js
@@ -0,0 +1,7 @@
+import Home from '../theme/components/Home';
+
+let custom_extensions = {
+  Home,
+};
+
+export default custom_extensions;
