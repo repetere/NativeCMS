e9a2746cd76c86c170e2baccb8ddbe54c3cfbdd9
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-10-26 18:15:47 -0400

    working on more view

--- app/extensions/More/index.js
+++ app/extensions/More/index.js
@@ -4,195 +4,82 @@
  * @flow
  */
 //TO DO: https://github.com/clh161/react-native-easy-grid-view
-import React, {
-  Component
-} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ListView,
-  ScrollView,
-  Image,
-  Platform
-} from 'react-native';
-// import styles from '../../components/Styles/shared';
-import {
-  Button, PricingCard
-} from 'react-native-elements';
+import React, { Component, } from 'react';
+import { StyleSheet, Text, View, ListView, ScrollView, Image, Platform, TouchableHighlight, } from 'react-native';
+import { Button, PricingCard, } from 'react-native-elements';
 import PopupDialog, { SlideAnimation } from 'react-native-popup-dialog';
+import SortableListView from 'react-native-sortable-listview';
+import styles from '../../components/Styles/shared';
+import colorStyles from '../../components/Styles/colors';
 
-import GridView from 'react-native-easy-grid-view';
-class Example extends Component {
-  constructor(props) {
-    super(props);
-    var ds = new GridView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-    });
-    this.state = {
-      dataSource: ds.cloneWithCells([
-        {
-          text: 1,
-          backgroundColor: '#f00',
-        }, {
-          text: 2,
-          backgroundColor: '#0f0',
-        }, {
-          text: 3,
-          backgroundColor: '#00f',
-        }, {
-          text: 4,
-          backgroundColor: '#f0f',
-        }, {
-          text: 5,
-          backgroundColor: '#fff',
-        }, {
-          text: 6,
-          backgroundColor: '#000',
-        } ], 1),
-      cellWidth: 0,
-      cellHeight: 0,
-    };
+let data = {
+  hello: { text: 'world', },
+  how: { text: 'are you', },
+  test: { text: 123, },
+  this: { text: 'is', },
+  a: { text: 'a', },
+  real: { text: 'real', },
+  drag: { text: 'drag and drop', },
+  bb: { text: 'bb', },
+  cc: { text: 'cc', },
+  dd: { text: 'dd', },
+  ee: { text: 'ee', },
+  ff: { text: 'ff', },
+  gg: { text: 'gg', },
+  hh: { text: 'hh', },
+  ii: { text: 'ii', },
+  jj: { text: 'jj', },
+  kk: { text: 'kk', },
+};
+
+let order = Object.keys(data); //Array of keys
+
+class RowComponent extends Component{
+  render() {
+    return (<TouchableHighlight underlayColor={'#eee'} style={{ padding: 25, backgroundColor: "#F8F8F8", borderBottomWidth: 1, borderColor: '#eee' }} {...this.props.sortHandlers}>
+      <Text>{this.props.data.text}</Text>
+    </TouchableHighlight>);
   }
-  _renderCell(cell) {
-    /**
-    <PricingCard
-  color='#4f9deb'
-  title='Free'
-  price='$0'
-  info={['1 User', 'Basic Support', 'All Core Features']}
-  button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
-/> */
-    return (
-      <View onLayout={event => {
-        // console.log('event.nativeEvent.layout',event.nativeEvent.layout)
-        // console.log('cell',cell)
-        var width = event.nativeEvent.layout.width;
-        if (this.state.cellWidth != width) {
-          this.setState({ cellWidth: width, });
-        }
-        if (this.state.cellHeight != width) {
-          this.setState({ cellHeight: width, });
-        }
-      } }>
-        <View style={{
-          width: this.state.cellWidth,
-          height: this.state.cellHeight,
-          justifyContent: 'center',
-          backgroundColor: cell.backgroundColor,
-        }}
-          // resizeMode={Image.resizeMode.stretch}
-          // source={cell.image}
-          >
-     <PricingCard
-  color='#4f9deb'
-  title='Free'
-  price='$0'
-  info={['1 User', 'Basic Support', 'All Core Features']}
-  button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
-/>
-          {/*}     <Text style={{
-            backgroundColor: '#0004',
-            textAlign: 'center',
-            color: '#fff',
-            fontSize: 24,
-          }}>{cell.text}</Text>
-  */}
-        </View>
-      </View>
-    );
+}
+
+class More extends Component {
+  constructor(){
+    super(...arguments);
+    this.state = {
+      // ranattr:'ok',
+    };
   }
   render() {
     return (
-      <ScrollView style={{
-        flex: 1,
-        alignSelf: 'stretch',
-        flexDirection: 'column',
-        padding: 8,
-      }}
-      contentContainerStyle={{
-        paddingVertical: 20,
-        position: 'relative',
-      }}>
-        <View style={{
-          flex: 1,
-          alignSelf: 'stretch',
-        }}>
-          
-          <Button
-            title="Open Dialog"
-            onPress={() => {
-              this.popupDialog.openDialog();
-            }}
-          />
-          <PricingCard
-            color='#4f9deb'
-            title='Free'
-            price='$0'
-            info={['1 User', 'Basic Support', 'All Core Features']}
-            button={{
-              title: 'GET STARTED', icon: 'flight-takeoff',
-            }}
-            />
-          <Text>grid view</Text>
-          <Text>grid view</Text>
-          <Text>grid view</Text>
-          <Text>grid view</Text>
-        </View>
-        <View style={{
-          flex: 1,
-          alignSelf: 'stretch',
-        }}>
-          <GridView dataSource={this.state.dataSource}
-            spacing={8}
-            horizontal={true}
-            style={{
-              padding: 0,
-            }}
-            renderCell={this._renderCell.bind(this) } />
-        </View>
-
-
-          <PopupDialog
-            ref={(popupDialog) => { this.popupDialog = popupDialog; }}
-            dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' }) }
-          >
-            <View>
-              <Text>Hello</Text>
-            </View>
-          </PopupDialog>
-      </ScrollView>
+      <View style={ styles.container }>
+        <Text style={ styles.heading }>In the apps app</Text>		
+        <Button
+          small
+          iconRight
+          icon={{ name: 'code', }}
+          title="Code" />
+        <Button
+          small
+          iconRight
+          icon={{ name: 'share-apple',  type: 'evilicon', }}
+          title="Share Apple" />
+        <Button
+          small
+          iconRight
+          icon={{ name: 'battery-full',  type: 'foundation', }}
+          title="Battery Full" />
+        <SortableListView
+          style={{flex: 1}}
+          data={data}
+          order={order}
+          onRowMoved={e => {
+            order.splice(e.to, 0, order.splice(e.from, 1)[0]);
+            this.forceUpdate();
+          }}
+          renderRow={row => <RowComponent data={row} />}
+        />
+      </View>
     );
   }
 }
-// class More extends Component {
-//   constructor(){
-//     super(...arguments);
-//     this.state = {
-//       ranattr:'ok',
-//     };
-//   }
-//   render() {
-//     return (
-//       <View style={ styles.container }>
-//         <Text style={ styles.heading }>In the apps app</Text>		
-//         <Button
-//           small
-//           iconRight
-//           icon={{ name: 'code', }}
-//           title="Code" />
-//         <Button
-//           small
-//           iconRight
-//           icon={{ name: 'share-apple',  type: 'evilicon', }}
-//           title="Share Apple" />
-//         <Button
-//           small
-//           iconRight
-//           icon={{ name: 'battery-full',  type: 'foundation', }}
-//           title="Battery Full" />
-//       </View>
-//     );
-//   }
-// }
-export default Example;
+export default More;
